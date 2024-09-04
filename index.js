7ecd2ee613bf1ad40d4e86e6cb86ba1ec108510d
TriBellCat <t_belen@u.pacific.edu>
2024-09-03 21:48:31 -0700

    Reworked evaluation, PST, weights, and worked on UI

--- index.js
+++ index.js
@@ -1,3 +1,7 @@
+/*
+  Global Variables
+*/
+
 let board;
 let game = new Chess();   //Initializes a new Chess game instance
 
@@ -6,186 +10,178 @@ let $status = $('#status');
 let $fen = $('#fen');
 let $pgn = $('#pgn');
 
-//Keeps track of how many pieces each side took
+//Keeps track of the pieces each side took
 let capturedPieces = {
-  white :[],
+  white: [],
   black: []
 };
 
-//Squares that will be highlighted later on
+//The color of squares that will be highlighted for legal moves
 let whiteSquareGrey = '#a9a9a9';
 let blackSquareGrey = '#696969';
 
-//Variables to add to the AI' scores
-let addMove = 1200;
-let addCapture = 1000;
+const depthLimit = 3; //Set a depth limit for the AI search
 
 //Each piece type has weights so that the board can evaluate them later
 const weights = {
-  'p': 100,
-  'n': 280,
-  'b': 320,
-  'r': 479,
-  'q': 929,
-  'k': 60000 //Should be the highest since the king is the most important piece
+  'p': 10,
+  'n': 30,
+  'b': 30,
+  'r': 50,
+  'q': 90,
+  'k': 900 //The highest since the king is the most important piece
 };
 
-//Piece square tables to assign an additional score delta to each piece based on its position on the board
-const pst = {
-'p':[
-            [ 100, 100, 100, 100, 105, 100, 100,  100],
-            [  78,  83,  86,  73, 102,  82,  85,  90],
-            [   7,  29,  21,  44,  40,  31,  44,   7],
-            [ -17,  16,  -2,  15,  14,   0,  15, -13],
-            [ -26,   3,  10,   9,   6,   1,   0, -23],
-            [ -22,   9,   5, -11, -10,  -2,   3, -19],
-            [ -31,   8,  -7, -37, -36, -14,   3, -31],
-            [   0,   0,   0,   0,   0,   0,   0,   0]
-        ],
-    'n': [ 
-            [-66, -53, -75, -75, -10, -55, -58, -70],
-            [ -3,  -6, 100, -36,   4,  62,  -4, -14],
-            [ 10,  67,   1,  74,  73,  27,  62,  -2],
-            [ 24,  24,  45,  37,  33,  41,  25,  17],
-            [ -1,   5,  31,  21,  22,  35,   2,   0],
-            [-18,  10,  13,  22,  18,  15,  11, -14],
-            [-23, -15,   2,   0,   2,   0, -23, -20],
-            [-74, -23, -26, -24, -19, -35, -22, -69]
-        ],
-    'b': [ 
-            [-59, -78, -82, -76, -23,-107, -37, -50],
-            [-11,  20,  35, -42, -39,  31,   2, -22],
-            [ -9,  39, -32,  41,  52, -10,  28, -14],
-            [ 25,  17,  20,  34,  26,  25,  15,  10],
-            [ 13,  10,  17,  23,  17,  16,   0,   7],
-            [ 14,  25,  24,  15,   8,  25,  20,  15],
-            [ 19,  20,  11,   6,   7,   6,  20,  16],
-            [ -7,   2, -15, -12, -14, -15, -10, -10]
-        ],
-    'r': [  
-            [ 35,  29,  33,   4,  37,  33,  56,  50],
-            [ 55,  29,  56,  67,  55,  62,  34,  60],
-            [ 19,  35,  28,  33,  45,  27,  25,  15],
-            [  0,   5,  16,  13,  18,  -4,  -9,  -6],
-            [-28, -35, -16, -21, -13, -29, -46, -30],
-            [-42, -28, -42, -25, -25, -35, -26, -46],
-            [-53, -38, -31, -26, -29, -43, -44, -53],
-            [-30, -24, -18,   5,  -2, -18, -31, -32]
-        ],
-    'q': [   
-            [  6,   1,  -8,-104,  69,  24,  88,  26],
-            [ 14,  32,  60, -10,  20,  76,  57,  24],
-            [ -2,  43,  32,  60,  72,  63,  43,   2],
-            [  1, -16,  22,  17,  25,  20, -13,  -6],
-            [-14, -15,  -2,  -5,  -1, -10, -20, -22],
-            [-30,  -6, -13, -11, -16, -11, -16, -27],
-            [-36, -18,   0, -19, -15, -15, -21, -38],
-            [-39, -30, -31, -13, -31, -36, -34, -42]
-        ],
-    'k': [  
-            [  4,  54,  47, -99, -99,  60,  83, -62],
-            [-32,  10,  55,  56,  56,  55,  10,   3],
-            [-62,  12, -57,  44, -67,  28,  37, -31],
-            [-55,  50,  11,  -4, -19,  13,   0, -49],
-            [-55, -43, -52, -28, -51, -47,  -8, -50],
-            [-47, -42, -43, -79, -64, -32, -29, -32],
-            [ -4,   3, -14, -50, -57, -18,  13,   4],
-            [ 17,  30,  -3, -14,   6,  -1,  40,  18]
-        ],
+/*
+  Piece Square Tables (PST)
+  Assigns an additional score delta to each piece based on its position on the board
+*/
+
+//PST for the player
+const pst_w = {
+  'p': [
+    [0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0],
+    [5.0, 5.0, 5.0, 5.0, 5.0, 5.0, 5.0, 5.0],
+    [1.0, 1.0, 2.0, 3.0, 3.0, 2.0, 1.0, 1.0],
+    [0.5, 0.5, 1.0, 2.5, 2.5, 1.0, 0.5, 0.5],
+    [0.0, 0.0, 0.0, 2.0, 2.0, 0.0, 0.0, 0.0],
+    [0.5, -0.5, -1.0, 0.0, 0.0, -1.0, -0.5, 0.5],
+    [0.5, 1.0, 1.0, -2.0, -2.0, 1.0, 1.0, 0.5],
+    [0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0]
+  ],
+  'n': [
+    [-5.0, -4.0, -3.0, -3.0, -3.0, -3.0, -4.0, -5.0],
+    [-4.0, -2.0, 0.0, 0.0, 0.0, 0.0, -2.0, -4.0],
+    [-3.0, 0.0, 1.0, 1.5, 1.5, 1.0, 0.0, -3.0],
+    [-3.0, 0.5, 1.5, 2.0, 2.0, 1.5, 0.5, -3.0],
+    [-3.0, 0.0, 1.5, 2.0, 2.0, 1.5, 0.0, -3.0],
+    [-3.0, 0.5, 1.0, 1.5, 1.5, 1.0, 0.5, -3.0],
+    [-4.0, -2.0, 0.0, 0.5, 0.5, 0.0, -2.0, -4.0],
+    [-5.0, -4.0, -3.0, -3.0, -3.0, -3.0, -4.0, -5.0]
+  ],
+  'b': [
+    [-2.0, -1.0, -1.0, -1.0, -1.0, -1.0, -1.0, -2.0],
+    [-1.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, -1.0],
+    [-1.0, 0.0, 0.5, 1.0, 1.0, 0.5, 0.0, -1.0],
+    [-1.0, 0.5, 0.5, 1.0, 1.0, 0.5, 0.5, -1.0],
+    [-1.0, 0.0, 1.0, 1.0, 1.0, 1.0, 0.0, -1.0],
+    [-1.0, 1.0, 1.0, 1.0, 1.0, 1.0, 1.0, -1.0],
+    [-1.0, 0.5, 0.0, 0.0, 0.0, 0.0, 0.5, -1.0],
+    [-2.0, -1.0, -1.0, -1.0, -1.0, -1.0, -1.0, -2.0]
+  ],
+  'r': [
+    [0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0],
+    [0.5, 1.0, 1.0, 1.0, 1.0, 1.0, 1.0, 0.5],
+    [-0.5, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, -0.5],
+    [-0.5, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, -0.5],
+    [-0.5, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, -0.5],
+    [-0.5, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, -0.5],
+    [-0.5, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, -0.5],
+    [0.0, 0.0, 0.0, 0.5, 0.5, 0.0, 0.0, 0.0]
+  ],
+  'q': [
+    [-2.0, -1.0, -1.0, -0.5, -0.5, -1.0, -1.0, -2.0],
+    [-1.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, -1.0],
+    [-1.0, 0.0, 0.5, 0.5, 0.5, 0.5, 0.0, -1.0],
+    [-0.5, 0.0, 0.5, 0.5, 0.5, 0.5, 0.0, -0.5],
+    [0.0, 0.0, 0.5, 0.5, 0.5, 0.5, 0.0, -0.5],
+    [-1.0, 0.5, 0.5, 0.5, 0.5, 0.5, 0.0, -1.0],
+    [-1.0, 0.0, 0.5, 0.0, 0.0, 0.0, 0.0, -1.0],
+    [-2.0, -1.0, -1.0, -0.5, -0.5, -1.0, -1.0, -2.0]
+  ],
+  'k': [
+    [-3.0, -4.0, -4.0, -5.0, -5.0, -4.0, -4.0, -3.0],
+    [-3.0, -4.0, -4.0, -5.0, -5.0, -4.0, -4.0, -3.0],
+    [-3.0, -4.0, -4.0, -5.0, -5.0, -4.0, -4.0, -3.0],
+    [-3.0, -4.0, -4.0, -5.0, -5.0, -4.0, -4.0, -3.0],
+    [-2.0, -3.0, -3.0, -4.0, -4.0, -3.0, -3.0, -2.0],
+    [-1.0, -2.0, -2.0, -2.0, -2.0, -2.0, -2.0, -1.0],
+    [2.0, 2.0, 0.0, 0.0, 0.0, 0.0, 2.0, 2.0],
+    [2.0, 3.0, 1.0, 0.0, 0.0, 1.0, 3.0, 2.0]
+  ],
 };
+
+//PST for the AI
+const pst_b = {
+  'p': pst_w['p'].slice().reverse(),
+  'n': pst_w['n'].slice().reverse(),
+  'b': pst_w['b'].slice().reverse(),
+  'r': pst_w['r'].slice().reverse(),
+  'q': pst_w['q'].slice().reverse(),
+  'k': pst_w['k'].slice().reverse(),
+};
+
 
 /* 
   AI 
 */
-//Function for the AI to make random moves
-function makeRandomMove() {
-  let possibleMoves = game.moves();
 
-  //game over
-  if (possibleMoves.length === 0) {
-    return;
-  }
+//Function to evaluate the current state of the board and returns a score
+//A positive score favors white, while a negative score favors black
+function evaluateBoard(board) {
+  let score = 0; 
 
-  let randomIdx = Math.floor(Math.random() * possibleMoves.length);
-  game.move(possibleMoves[randomIdx]);
-  board.position(game.fen());
-}
-
-//Function to evaluates the current state of the board and returns a score.
-//A positive score favors white, while a negative score favors black.
-function evaluateBoard(move) {
-  let score = 0; //Initializes the score to 0.
-
-  //Iterates over all squares of the chessboard.
+  //Iterates over all squares of the chessboard
   for (let i = 0; i < 8; i++) {
     for (let j = 0; j < 8; j++) {
-      const piece = game.get(String.fromCharCode(97 + j) + (8 - i)); //Gets the piece at the current square (j, i) and convert its coordinates to algebraic notation
-
-      //Checks if there's a piece on the square
-      if (piece) {
-        const pieceValue = weights[piece.type];                 //Gets the piece value from the weights object
-        const piecePositionValue = pst[piece.type][i][j];  //Gets the position value from the piece-square table
-
-        //Adds or subtract the piece value and position value to the score based on its color
-        if (piece.color === 'w') {
-          score += pieceValue + piecePositionValue;
-        } 
-        else {
-          score -= pieceValue + piecePositionValue;
-        }
-      }
+      //Adds the value of the piece on the current square to the total score
+      score += getPieceValue(board[i][j], i, j);
     }
-  }
-  
-  //Evaluate the number of legal moves for each side
-  const whiteMoves = game.moves({ verbose: true, legal: true }).filter(m => m.color === 'w').length;
-  const blackMoves = game.moves({ verbose: true, legal: true }).filter(m => m.color === 'b').length;
-
-  //Add scores to its movement; more moves is generally better
-  score += addMove * (whiteMoves - blackMoves);
-
-  //Adds more to score for capturing a piece
-  if (move && move.captured) {
-    const captureValue = weights[move.captured] * addCapture; 
-    score += (move.color === 'w' ? captureValue : -captureValue);
   }
 
   return score;
 }
 
-
-
-//Function to implement the minimax algorithm with alpha-beta pruning to determine the best move for the AI.
-function minimax(depth, alpha, beta, isMaximizingPlayer, lastMove) {
-  //If the depth limit is reached or game over, return the board evaluation
-  if (depth === 0 || game.game_over()) {
-    return evaluateBoard(lastMove);
+//Function to get the value of a piece based on its type and position
+function getPieceValue(piece, x, y) {
+  //Returns 0 if there is no piece on the square
+  if (piece === null) {
+    return 0;
   }
 
-  let possibleMoves = game.moves();                           //Get all possible moves for the current player.
-  let bestScore = isMaximizingPlayer ? -Infinity : Infinity;  //Initialize best score based on the current player.
+  let absoluteValue = getAbsoluteValue(piece, piece.color === 'w', x, y);      //Calculates the absolute value of the piece
+  return piece.color === 'w' ? absoluteValue : -absoluteValue;                 //Returns positive for white pieces and negative for black pieces
+}
 
-  //Iterate over all possible moves.
+//Helper function to calculate the absolute value of a piece based off its position on the board
+function getAbsoluteValue (piece, isWhite, x, y) {
+  let tables = isWhite ? pst_w : pst_b;                       //Selects the PST based on piece color
+  let value = weights[piece.type] + tables[piece.type][y][x]; //Calculates value from a base weight and position table
+
+  return value;
+}
+
+//Function to implement the minimax algorithm with alpha-beta pruning to determine the best move for the AI
+function minimax(depth, game, alpha, beta, isMaximizingPlayer) {
+  //If the depth limit is reached, return the board evaluation
+  if (depth === 0) {
+    return -evaluateBoard(game.board());
+  }
+
+  let possibleMoves = game.moves();                            //Get all possible moves for the current player
+  let bestScore = isMaximizingPlayer ? -Infinity : Infinity;  //Initialize best score based on the current player
+
+  //Iterate over all possible moves
   for (let i = 0; i < possibleMoves.length; i++) {
-    game.move(possibleMoves[i]);                                       //Makes the move on the board
-    let score = minimax(depth - 1, alpha, beta, !isMaximizingPlayer); //Recursively call minimax for the opponent, reducing depth and swapping player.
-    game.undo();                                                      //Undo the move to explore other possibilities
+    game.move(possibleMoves[i]);                                              //Makes all possible move on the board
+    let score = minimax(depth - 1, game, alpha, beta, !isMaximizingPlayer);   //Recursively calls minimax for the opponent, reducing depth and swapping player; setting the score to its results
+    game.undo();                                                              //Undoes the move
 
     //Update best score based on the current player.
     if (isMaximizingPlayer) {
       bestScore = Math.max(bestScore, score);
-      alpha = Math.max(alpha, bestScore);
-    }
+      alpha = Math.max(alpha, score);
+    } 
     else {
       bestScore = Math.min(bestScore, score);
-      beta = Math.min(beta, bestScore);
+      beta = Math.min(beta, score);
     }
 
-    //If beta is less than or equal to alpha, prune the branch.
     if (beta <= alpha) {
       break;
     }
   }
+
   return bestScore;
 }
 
@@ -193,33 +189,25 @@ function minimax(depth, alpha, beta, isMaximizingPlayer, lastMove) {
 function makeAIMove() {
   let bestMove = null;                //Keeps track of the best move
   let bestScore = -Infinity;          //Keeps track of the best score
-  let possibleMoves = game.moves({ verbose: true });   //Get all possible moves for the AI.
-  const depthLimit = 3;               //Set a depth limit for the AI search
-
-   //Sort moves to prioritize captures
-   possibleMoves.sort((a, b) => {
-    const aCaptureValue = a.captured ? weights[a.captured] : 0;
-    const bCaptureValue = b.captured ? weights[b.captured] : 0;
-    return bCaptureValue - aCaptureValue; //Descending order
-  });
+  let possibleMoves = game.moves();   //Get all possible moves for the AI
 
   //Iterate over all possible moves.
   for (let i = 0; i < possibleMoves.length; i++) {
-    game.move(possibleMoves[i]); //Makes the move on the board.
-    
-    //Calls minimax to get the score of the move
-    let score = minimax(depthLimit - 1, -Infinity, Infinity, false, possibleMoves[i]);  
-    
-    game.undo(); //Undo the move to explore other possibilities.
+    game.move(possibleMoves[i]); //Makes the move on the board
 
-    //Update best move and score if current score is better.
+    //Calls minimax to get the score of the move
+    let score = minimax(depthLimit - 1, game, -Infinity, Infinity, false);
+
+    game.undo();  //Undo the move to explore other possibilities
+
+    //Update best move and score if current score is better
     if (score > bestScore) {
       bestScore = score;
       bestMove = possibleMoves[i];
     }
   }
 
-  //If a best move was found, make the move on the board.
+  //If a best move was found, make the move on the board
   if (bestMove) {
     let move = game.move(bestMove);
     board.position(game.fen());
@@ -252,8 +240,8 @@ function greySquare(square) {
 //Checks if a piece was captured and update the the list of captured pieces
 function updateCapturedPieces(move) {
   if (move.captured) {
-      let color = move.color === 'w' ? 'black' : 'white';
-      capturedPieces[color].push(move.captured);
+    let color = move.color === 'w' ? 'black' : 'white';
+    capturedPieces[color].push(move.captured);
   }
 }
 
@@ -269,18 +257,18 @@ function updateCapturedDisplay() {
 
 //Function to reset the game
 function resetGame() {
-  game.reset();  
+  game.reset();
   board.start();
-  capturedPieces = { white: [], black: [] }; 
-  updateStatus(); 
-  updateCapturedDisplay(); 
+  capturedPieces = { white: [], black: [] };
+  updateStatus();
+  updateCapturedDisplay();
 }
 
 document.getElementById("reset-button").addEventListener("click", resetGame);
 
 //Function to handle the start of a piece drag on the board
 function onDragStart(source, piece, position, orientation) {
-  //do not pick up pieces if the game is over or only pick up pieces for White
+  //Do not pick up pieces if the game is over or only pick up pieces for White
   if (game.game_over() || piece.search(/^b/) !== -1) {
     return false;
   }
@@ -301,28 +289,28 @@ function onDrop(source, target) {
     return 'snapback';
   }
 
-  window.setTimeout(makeAIMove, 250); //make random legal move for black
+  window.setTimeout(makeAIMove, 250); //Starts movement for AI
 
   updateCapturedPieces(move);
   updateStatus(); //Update the game status after each move
 }
 
 function onMouseoverSquare(square, piece) {
-  //get list of possible moves for this square
+  //Get list of possible moves for this square
   let moves = game.moves({
     square: square,
     verbose: true
   })
 
-  //exit if there are no moves available for this square
+  //Exit if there are no moves available for this square
   if (moves.length === 0) {
     return;
   }
 
-  //highlight the square they moused over
+  //Highlight the square they moused over
   greySquare(square)
 
-  //highlight the possible squares for this piece
+  //Highlight the possible squares for this piece
   for (let i = 0; i < moves.length; i++) {
     greySquare(moves[i].to)
   }
@@ -351,7 +339,7 @@ function updateStatus() {
     status = `Game over, ${moveColor} is in checkmate.`;
   }
   else if (game.in_draw()) {
-    status = 'Game over, drawn position';
+    status = `Game over, drawn position`;
   }
   else {
     status = `${moveColor}'s turn`;
